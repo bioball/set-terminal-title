9cceedf12d536a28fa7cf863f695c40dabe3ade4
Dan Chao <dan@play-i.com>
2015-04-20 11:22:56 -0700

    set dat terminal title

---
+++ README.md
@@ -0,0 +1,31 @@
+# Set Terminal Title
+
+Set the title of your terminal window.
+
+## Example
+
+```js
+var setTerminalTitle = require('setTerminalTitle');
+setTerminalTitle('Wallaby Wallace', { verbose: true });
+// => Setting terminal title as Wallaby Wallace
+```
+
+## Usage
+
+Syntax is `setTerminalTitle(<name>, <opts>)`, where `opts` is an object.
+
+## Options
+
+### Verbose
+
+Type: `Boolean`
+Default: `false`
+
+Will log out that you're setting the terminal title
+
+### Logger
+
+Type: `Object`
+Default: `console`
+
+The logger instance to use. Needs to have an `info` method.
---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * setTerminalTitle
+ * Sets the terminal title for your process.
+ *
+ * @param  {String}  name          the name to set as your title
+ * @param  {Object}  opts
+ * @param  {Boolean} opts.verbose  toggle verbose mode.
+ * @param  {Boolean} opts.logger   The logger to use. Needs to have an `info` method.
+ * @return {Undefined}
+ */
+module.exports = function setTerminalTitle(name, opts){
+  opts = opts || {};
+  opts.logger = opts.logger || console;
+  if (opts.verbose) {
+    logger.info("Setting terminal title as " + name);
+  }
+  process.stdout.write("\033]0;" + name + "\007");
+};
